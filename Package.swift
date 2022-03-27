4a71b4bd60c24e03e6891f07dc427eb0983f68e8
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-03-27 07:29:28 -0400

    iOS support

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftTreeSitter",
-    platforms: [.macOS(.v10_13)],
+    platforms: [.macOS(.v10_13), .iOS(.v11)],
     products: [
         .library(name: "SwiftTreeSitter", targets: ["SwiftTreeSitter"]),
     ],
     dependencies: [
-      .package(url: "https://github.com/krzyzanowskim/tree-sitter-xcframework", from: "0.206.2")
+      .package(url: "https://github.com/krzyzanowskim/tree-sitter-xcframework", from: "0.206.7")
     ],
     targets: [
       .target(name: "SwiftTreeSitter", dependencies: ["tree_sitter"]),
