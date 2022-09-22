b1eceacc1af07e382b25252999765a155d284530
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-09-21 20:39:34 -0400

    Tutorial: edits

---
+++ Sources/SwiftTreeSitter/Documentation.docc/Code/using-essentials-3-2-example.swift
@@ -0,0 +1,23 @@
+import SwiftTreeSitter
+import TreeSitterSwift
+
+let language = Language(language: tree_sitter_swift())
+
+let parser = Parser()
+try parser.setLanguage(language)
+
+let source = """
+func hello() {
+    print("hello from tree-sitter")
+}
+"""
+
+let tree = parser.parse(source)
+
+print("tree: ", tree)
+
+let newSource = """
+func hello() {
+    print("hello from SwiftTreeSitter")
+}
+"""
---
+++ Sources/SwiftTreeSitter/Documentation.docc/Code/using-essentials-3-6-example.swift
@@ -0,0 +1,38 @@
+import SwiftTreeSitter
+import TreeSitterSwift
+
+let language = Language(language: tree_sitter_swift())
+
+let parser = Parser()
+try parser.setLanguage(language)
+
+let source = """
+func hello() {
+    print("hello from tree-sitter")
+}
+"""
+
+let tree = parser.parse(source)
+
+print("tree: ", tree)
+
+let newSource = """
+func hello() {
+    print("hello from SwiftTreeSitter")
+}
+"""
+
+let edit = InputEdit(startByte: 34,
+                     oldEndByte: 45,
+                     newEndByte: 49,
+                     startPoint: Point(row: 1, column: 22),
+                     oldEndPoint: Point(row: 1, column: 33),
+                     newEndPoint: Point(row: 1, column: 37))
+
+tree.edit(edit)
+
+let newTree = parser.parse(tree, string: newSource)
+
+let changedRanges = existingTree.changedRanges(newTree)
+
+print("changes: ", changedRanges)
