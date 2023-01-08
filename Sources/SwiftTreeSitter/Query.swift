29daf49489de020b049915018668e96b46741f75
Dimitris Apostolou <dimitris.apostolou@icloud.com>
2023-01-09 00:20:08 +0200

    Fix typos

--- Sources/SwiftTreeSitter/Query.swift
+++ Sources/SwiftTreeSitter/Query.swift
@@ -41,7 +41,7 @@ public enum QueryPredicateError: Error {
 ///
 /// Typically, query definitions are stored in a `.scm` file.
 ///
-/// Tree-sitter's official documenation: [Pattern Matching with Queries](https://tree-sitter.github.io/tree-sitter/using-parsers#pattern-matching-with-queries)
+/// Tree-sitter's official documentation: [Pattern Matching with Queries](https://tree-sitter.github.io/tree-sitter/using-parsers#pattern-matching-with-queries)
 public class Query {
     let internalQuery: OpaquePointer
     let predicateList: [[Predicate]]
