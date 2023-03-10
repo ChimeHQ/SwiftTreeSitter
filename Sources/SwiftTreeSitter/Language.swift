e294b5e1d65a2684d7ca08c428bc63973a8991ac
Jared Sorge <jared@jsorge.net>
2023-03-10 09:26:07 -0800

    Refactor directory provider

--- Sources/SwiftTreeSitter/Language.swift
+++ Sources/SwiftTreeSitter/Language.swift
@@ -1,21 +1,18 @@
 import Foundation
 import tree_sitter
 
-/// This is a closure whose output URL is to the directory which has the language `scm` files in its hierarchy. The
-/// ultimate search is recursive; if the resource files are embedded within hierarchy
-/// `Resources.bundle/Contents/Resources/queries`, giving the path to `Resources.bundle` will result in the files
-/// being found.
-public typealias DirectoryProvider = () -> URL
-
 public struct Language {
+    /// This is a closure whose output URL is to the directory which has the language `scm` files.
+    public typealias DirectoryProvider = () -> URL
+
     public var tsLanguage: UnsafePointer<TSLanguage>
     public var directoryProvider: DirectoryProvider?
 
     /// Creates an instance.
     /// - Parameters:
     ///   - language: The language to parse.
-    ///   - directoryProvider: An optional closure which would return a `URL` that can be searched for resources
-    ///   used by the language (such as highlights.scm).
+    ///   - directoryProvider: An optional closure which would return a directory `URL` to the container of the
+    ///   language's `scm` resources.
     public init(language: UnsafePointer<TSLanguage>, directoryProvider: DirectoryProvider? = nil) {
         self.tsLanguage = language
         self.directoryProvider = directoryProvider
@@ -24,10 +21,10 @@ public struct Language {
     /// Creates an instance.
     /// - Parameters:
     ///   - language: The language to parse.
-    ///   - languageName: The name of the language. This is used to attempt to find the embeeded resource directory
+    ///   - languageName: The name of the language. This is used to attempt to find the embedded resource directory
     ///    for the language (which would contain files like highlights.scm).
     public init(language: UnsafePointer<TSLanguage>, languageName: String) {
-        self.init(language: language, directoryProvider: embeddedResourceProvider(named: languageName))
+        self.init(language: language, directoryProvider: Language.embeddedResourceProvider(named: languageName))
     }
 }
 
@@ -83,33 +80,6 @@ extension Language: Equatable {
     }
 }
 
-/// For languages where the resources are embedded in the app's bundle, this function returns the directory provider
-/// that finds the location of the resource's containing folder on disk. The match is fuzzy so if a Swift Package
-/// Manager's resource bundle is in use it could take the shape of `PackageName_PackageName.bundle` in which case
-/// this function only needs `PackageName` as its input to find the path to the bundle.
-///
-/// This function also normalizes for tests so that resources can be found while under test. `Bundle.main` is different
-/// in an app context vs an XCTest context.
-/// - Parameter resourceDirectory: The name of the directory or bundle to search for embedded within the main bundle.
-/// - Returns: If found, returns the directory provider closure.
-private func embeddedResourceProvider(named resourceDirectory: String) -> DirectoryProvider? {
-    let fileManager = FileManager.default
-    var bundle = Bundle.main
-
-    if bundle.isXCTestRunner {
-        bundle = Bundle.allBundles
-            .first(where: { $0.bundlePath.components(separatedBy: "/").last!.contains("Tests.xctest") == true })!
-    }
-
-    guard
-        let foundBundleURL = try? fileManager
-            .contentsOfDirectory(at: bundle.bundleURL, includingPropertiesForKeys: nil)
-            .first(where: { $0.lastPathComponent.contains(resourceDirectory) })
-    else { return nil }
-
-    return { foundBundleURL }
-}
-
 public extension Language {
     var highlightsFileURL: URL? {
         guard let url = directoryProvider?() else { return nil }
@@ -131,17 +101,66 @@ public extension Language {
                 options: [.skipsHiddenFiles]
             )
 
+            return contents.first(where: { $0.lastPathComponent == filename })
+        }
+        catch {
+            return nil
+        }
+    }
+}
+
+extension Language {
+    /// For languages where the resources are embedded in the app's bundle, this function returns the directory provider
+    /// that finds the location of the resource's containing folder on disk. The match is fuzzy so if a Swift Package
+    /// Manager's resource bundle is in use it could take the shape of `PackageName_PackageName.bundle` in which case
+    /// this function only needs `PackageName` as its input to find the path to the bundle.
+    ///
+    /// This function also normalizes for tests so that resources can be found while under test. `Bundle.main` is different
+    /// in an app context vs an XCTest context.
+    /// - Parameter resourceDirectory: The name of the directory or bundle to search for embedded within the main bundle.
+    /// - Returns: If found, returns the directory provider closure.
+    static func embeddedResourceProvider(named resourceDirectory: String) -> Language.DirectoryProvider? {
+        let fileManager = FileManager.default
+        var bundle = Bundle.main
+
+        if bundle.isXCTestRunner {
+            bundle = Bundle.allBundles
+                .first(where: { $0.bundlePath.components(separatedBy: "/").last!.contains("Tests.xctest") == true })!
+        }
+
+        guard
+            let foundBundleURL = try? fileManager
+                .contentsOfDirectory(at: bundle.bundleURL, includingPropertiesForKeys: nil)
+                .first(where: { $0.lastPathComponent.contains(resourceDirectory) }),
+            let resourceDirURL = searchForFileExtension("scm", startingIn: foundBundleURL)
+        else { return nil }
+
+        return { resourceDirURL }
+    }
+
+    private static func searchForFileExtension(_ ext: String, startingIn directory: URL) -> URL? {
+        let fileManager = FileManager.default
+
+        do {
+            let contents = try fileManager.contentsOfDirectory(
+                at: directory,
+                includingPropertiesForKeys: [.nameKey, .isDirectoryKey],
+                options: [.skipsHiddenFiles]
+            )
+
             for item in contents {
                 var isDirectory: ObjCBool = false
                 fileManager.fileExists(atPath: item.path, isDirectory: &isDirectory)
 
-                if isDirectory.boolValue {
-                    if let foundURL = findFile(filename, in: item.standardizedFileURL) {
-                        return foundURL
+                if isDirectory.boolValue == false {
+                    if item.lastPathComponent.contains(ext) {
+                        var urlComponents = URLComponents(url: item, resolvingAgainstBaseURL: false)!
+                        urlComponents.path = (urlComponents.path as NSString).deletingLastPathComponent
+                        return urlComponents.url
                     }
                 } else {
-                    if item.lastPathComponent == filename {
-                        return item
+                    if let foundURL = searchForFileExtension(ext, startingIn: item.standardizedFileURL) {
+                        return foundURL
                     }
                 }
             }
