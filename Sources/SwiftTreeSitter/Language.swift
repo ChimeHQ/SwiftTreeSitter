8b55ee60fafd84336a7b76512180941fc357e3f8
Jared Sorge <jsorge@users.noreply.github.com>
2023-03-11 03:27:42 -0800

    Add language support for finding resources (#15)
    * Add support for language resources
    
    * Search a resource path for a given file
    
    * Add extra test
    
    * Build out directory provider
    
    * API refinement
    
    * Refactor directory provider
    
    * Longer but hopefully more sensible name
    
    * languageName -> name
    
    * simplify
    
    * Fix WASM tests
    
    * Fix tests on tvos

--- Sources/SwiftTreeSitter/Language.swift
+++ Sources/SwiftTreeSitter/Language.swift
@@ -2,11 +2,32 @@ import Foundation
 import tree_sitter
 
 public struct Language {
-    public var tsLanguage: UnsafePointer<TSLanguage>
+    /// This is a closure whose output URL is to the directory which has the language `scm` files.
+    public typealias DirectoryProvider = () -> URL
 
-    public init(language: UnsafePointer<TSLanguage>) {
+    public var tsLanguage: UnsafePointer<TSLanguage>
+    public var directoryProvider: DirectoryProvider?
+
+    /// Creates an instance.
+    /// - Parameters:
+    ///   - language: The language to parse.
+    ///   - directoryProvider: An optional closure which would return a directory `URL` to the container of the
+    ///   language's `scm` resources.
+    public init(language: UnsafePointer<TSLanguage>, directoryProvider: DirectoryProvider? = nil) {
         self.tsLanguage = language
+        self.directoryProvider = directoryProvider
     }
+
+    #if !os(WASI)
+    /// Creates an instance.
+    /// - Parameters:
+    ///   - language: The language to parse.
+    ///   - languageName: The name of the language. This is used to attempt to find the embedded resource directory
+    ///    for the language (which would contain files like highlights.scm).
+    public init(language: UnsafePointer<TSLanguage>, name: String) {
+        self.init(language: language, directoryProvider: Language.embeddedResourceProvider(named: name))
+    }
+    #endif
 }
 
 extension Language {
@@ -56,9 +77,94 @@ extension Language {
 }
 
 extension Language: Equatable {
+    public static func ==(lhs: Language, rhs: Language) -> Bool {
+        return lhs.tsLanguage == rhs.tsLanguage
+    }
+}
+
+public extension Language {
+    var highlightsFileURL: URL? {
+        return directoryProvider?().appendingPathComponent("highlights.scm")
+    }
+
+    var injectionsFileURL: URL? {
+        return directoryProvider?().appendingPathComponent("injections.scm")
+    }
 }
 
 #if !os(WASI)
+extension Language {
+    /// For languages where the resources are embedded in the app's bundle, this function returns the directory provider
+    /// that finds the location of the resource's containing folder on disk. The match is fuzzy so if a Swift Package
+    /// Manager's resource bundle is in use it could take the shape of `PackageName_PackageName.bundle` in which case
+    /// this function only needs `PackageName` as its input to find the path to the bundle.
+    ///
+    /// This function also normalizes for tests so that resources can be found while under test. `Bundle.main` is different
+    /// in an app context vs an XCTest context.
+    /// - Parameter resourceDirectory: The name of the directory or bundle to search for embedded within the main bundle.
+    /// - Returns: If found, returns the directory provider closure.
+    static func embeddedResourceProvider(named resourceDirectory: String) -> Language.DirectoryProvider? {
+        let fileManager = FileManager.default
+        var bundle = Bundle.main
+
+        if bundle.isXCTestRunner {
+            bundle = Bundle.allBundles
+                .first(where: { $0.bundlePath.components(separatedBy: "/").last!.contains("Tests.xctest") == true })!
+        }
+
+        guard
+            let foundBundleURL = try? fileManager
+                .contentsOfDirectory(at: bundle.bundleURL, includingPropertiesForKeys: [.isDirectoryKey])
+                .filter({ (try? $0.resourceValues(forKeys: [.isDirectoryKey]).isDirectory) == true })
+                .first(where: { $0.lastPathComponent.contains(resourceDirectory) }),
+            let resourceDirURL = searchForDirectoryContainingFileExtension("scm", startingIn: foundBundleURL)
+        else { return nil }
+
+        return { resourceDirURL }
+    }
+
+    private static func searchForDirectoryContainingFileExtension(_ ext: String, startingIn directory: URL) -> URL? {
+        let fileManager = FileManager.default
+
+        do {
+            let contents = try fileManager.contentsOfDirectory(
+                at: directory,
+                includingPropertiesForKeys: [.nameKey, .isDirectoryKey],
+                options: [.skipsHiddenFiles]
+            )
+
+            for item in contents {
+                var isDirectory: ObjCBool = false
+                fileManager.fileExists(atPath: item.path, isDirectory: &isDirectory)
+
+                if isDirectory.boolValue == false {
+                    if item.lastPathComponent.contains(ext) {
+                        var urlComponents = URLComponents(url: item, resolvingAgainstBaseURL: false)!
+                        urlComponents.path = (urlComponents.path as NSString).deletingLastPathComponent
+                        return urlComponents.url
+                    }
+                } else {
+                    if let foundURL = searchForDirectoryContainingFileExtension(ext, startingIn: item.standardizedFileURL) {
+                        return foundURL
+                    }
+                }
+            }
+        }
+        catch {
+            return nil
+        }
+
+        return nil
+    }
+}
+
+private extension Bundle {
+    var isXCTestRunner: Bool {
+        guard NSClassFromString("XCTest") != nil else { return false }
+        return bundlePath.contains("/Developer/Library/Xcode/Agents")
+    }
+}
+
 public extension Language {
     /// Construct a query object from data in a file.
     func query(contentsOf url: URL) throws -> Query {
---
+++ Tests/SwiftTreeSitterTests/LanguageTests.swift
@@ -0,0 +1,84 @@
+#if !os(WASI)
+import Foundation
+import TestTreeSitterSwift
+import SwiftTreeSitter
+import XCTest
+
+final class LanguageTests: XCTestCase {
+    private let fileManager = FileManager.default
+
+    func testFileFoundInBundle() throws {
+        let filename = "highlights.scm"
+        let bundlePath = "Swift.bundle/Contents/Resources/queries"
+        try setupFile(filename, in: bundlePath)
+
+        let language = Language(
+            language: tree_sitter_swift(),
+            name: "Swift"
+        )
+        XCTAssertNotNil(language.highlightsFileURL)
+
+        try removeFile(filename, in: bundlePath)
+    }
+
+    func testFileFoundInResourceDir() throws {
+        let filename = "injections.scm"
+        let dirPath = "Markdown/queries"
+        try setupFile(filename, in: dirPath)
+
+        let language = Language(
+            language: tree_sitter_swift(),
+            name: "Markdown"
+        )
+        XCTAssertNotNil(language.injectionsFileURL)
+
+        try removeFile(filename, in: dirPath)
+    }
+
+    func testMatchHappensWhenMultiplePathsShareCommonName() throws {
+        let filename = "injections.scm"
+        let jsonDirPath = "JSON/queries"
+        let json5DirPath = "JSON5/queries"
+        try setupFile(filename, in: jsonDirPath)
+        try setupFile(filename, in: json5DirPath)
+
+        let language = Language(
+            language: tree_sitter_swift(),
+            name: "JSON"
+        )
+        XCTAssertNotNil(language.injectionsFileURL)
+        XCTAssertTrue(language.injectionsFileURL!.absoluteString.contains("JSON/queries"))
+
+        try removeFile(filename, in: jsonDirPath)
+        try removeFile(filename, in: json5DirPath)
+    }
+
+    private func setupFile(_ filename: String, in directoryPath: String) throws {
+        let dir = Bundle.test.bundlePath.appending("/\(directoryPath)")
+        let filePath = "\(dir)/\(filename)"
+
+        if fileManager.fileExists(atPath: dir, isDirectory: nil) == false {
+            try fileManager.createDirectory(atPath: dir, withIntermediateDirectories: true)
+        }
+
+        if fileManager.createFile(atPath: filePath, contents: nil) == false {
+            XCTFail("could not write file")
+        }
+    }
+
+    private func removeFile(_ filename: String, in directoryPath: String) throws {
+        let filePath = Bundle.test.bundlePath.appending("/\(directoryPath)/\(filename)")
+        try fileManager.removeItem(atPath: filePath)
+    }
+}
+
+private extension Bundle {
+    static var test: Bundle {
+        return Bundle.allBundles
+            .first(where: { $0.bundlePath.components(separatedBy: "/")
+                .last!
+                .contains("Tests.xctest") == true }
+            )!
+    }
+}
+#endif
